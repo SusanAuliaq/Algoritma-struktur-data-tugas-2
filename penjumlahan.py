476ae754a8c8ea759884d20b500efbbf5a792916
SusanAuliaq <susanauliaa52@gmail.com>
2024-09-13 11:29:15 +0800

    Update penjumlahan.py

--- penjumlahan.py
+++ penjumlahan.py
@@ -1,1 +1,4 @@
-https://drive.google.com/file/d/1nfemwJiAIXioMufLjYETTdBOfWAFxPcc/view?usp=drivesdk
+bilangan1 = float(input("Masukkan bilangan pertama: "))
+bilangan2 = float(input("Masukkan bilangan kedua: "))
+hasil = bilangan1 + bilangan2
+print(f"Hasil penjumlahan {bilangan1} + {bilangan2} = {hasil}")
